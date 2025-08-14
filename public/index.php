3ebbdb0212601306db265936e02c62147bdf0f53
Petras Vizgintas <38919361+PetrasViz@users.noreply.github.com>
2025-08-14 11:47:59 +0300

    Implement basic route map and dynamic request dispatcher

--- public/index.php
+++ public/index.php
@@ -20,65 +20,12 @@ $requireLogin = function () use ($user) {
     }
 };
 
-switch ($uri) {
-    case '/':
-        include __DIR__ . '/../src/views/home.php';
-        break;
-    case '/login':
-        if ($method === 'POST') {
-            $auth->login();
-        } else {
-            $auth->showLoginForm();
-        }
-        break;
-    case '/register':
-        if ($method === 'POST') {
-            $auth->register();
-        } else {
-            $auth->showRegisterForm();
-        }
-        break;
-    case '/forgot':
-        if ($method === 'POST') {
-            $auth->sendResetLink();
-        } else {
-            $auth->showForgotForm();
-        }
-        break;
-    case '/logout':
-        $auth->logout();
-        break;
-    case '/auctions':
-        $requireLogin();
-        include __DIR__ . '/../src/views/auctions/index.php';
-        break;
-    case '/auction-history':
-        $requireLogin();
-        include __DIR__ . '/../src/views/auctions/history.php';
-        break;
-    case '/event-history':
-        $requireLogin();
-        include __DIR__ . '/../src/views/events/history.php';
-        break;
-    case '/profile':
-        $requireLogin();
-        if ($method === 'POST') {
-            $profile->update();
-        } else {
-            $profile->show();
-        }
-        break;
-    case '/management':
-        $requireLogin();
-        if ($user['role'] === 'guild_member') {
-            http_response_code(403);
-            echo 'Forbidden';
-        } else {
-            include __DIR__ . '/../src/views/management/index.php';
-        }
-        break;
-    default:
-        http_response_code(404);
-        echo 'Not Found';
-        break;
+$routes = require __DIR__ . '/../src/routes/web.php';
+
+if (isset($routes[$uri][$method])) {
+    $action = $routes[$uri][$method];
+    $action();
+} else {
+    http_response_code(404);
+    echo 'Not Found';
 }
